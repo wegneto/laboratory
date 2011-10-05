42bb10791ba6477773727aaab89655422654ebbf
Wilson Guimarães <wegneto@gmail.com>
2011-10-05 15:21:28 -0300

    Passo 08 - Exception - Inclusão da restrição de limite da turma.

--- inscricao/src/main/java/br/gov/serpro/inscricao/Turma.java
+++ inscricao/src/main/java/br/gov/serpro/inscricao/Turma.java
@@ -6,16 +6,24 @@ import javax.inject.Inject;
 
 import org.slf4j.Logger;
 
+import br.gov.frameworkdemoiselle.util.ResourceBundle;
+
 public class Turma {
 
 	@Inject
 	private Logger logger;
 	
+	@Inject
+	private ResourceBundle messages;
+	
 	private ArrayList<String> alunos = new ArrayList<String>();
 
 	public void matricular(String aluno) {
+		if (estaMatriculado(aluno) || alunos.size() >= 5) {
+			throw new RuntimeException();
+		}
 		alunos.add(aluno);
-		logger.info("Cadastro realizado com sucesso");
+		logger.info(messages.getString("cadastro.aluno.sucesso", aluno));
 	}
 
 	public boolean estaMatriculado(String aluno) {
--- inscricao/src/test/java/br/gov/serpro/inscricao/TurmaTest.java
+++ inscricao/src/test/java/br/gov/serpro/inscricao/TurmaTest.java
@@ -1,5 +1,7 @@
 package br.gov.serpro.inscricao;
 
+import javax.inject.Inject;
+
 import junit.framework.Assert;
 
 import org.junit.Test;
@@ -10,15 +12,47 @@ import br.gov.frameworkdemoiselle.junit.DemoiselleRunner;
 @RunWith(DemoiselleRunner.class)
 public class TurmaTest {
 	
+	@Inject
+	private Turma turma;
+	
 	@Test
 	public void cadastrarComSucesso() {
-		Turma turma = new Turma();
 		
 		String aluno = "Aluno 1";
 		
 		turma.matricular(aluno);
 		
 		Assert.assertTrue(turma.estaMatriculado(aluno));
+		
+	}
+	
+	@Test
+	public void falhaAoCadastrarDuplicado() {
+		String aluno = "Aluno 1";
+		
+		turma.matricular(aluno);
+		
+		try {
+			turma.matricular(aluno);
+			Assert.fail();
+		} catch (RuntimeException e) {
+		}
+		
+	}
+	
+	@Test
+	public void falhaAoCadastrarAcimaDoLimite() {
+		turma.matricular("Aluno 1");
+		turma.matricular("Aluno 2");
+		turma.matricular("Aluno 3");
+		turma.matricular("Aluno 4");
+		turma.matricular("Aluno 5");
+		
+		try {
+			turma.matricular("Aluno 6");
+			Assert.fail();
+		} catch (RuntimeException e) {
+		}
 		
 	}
 
