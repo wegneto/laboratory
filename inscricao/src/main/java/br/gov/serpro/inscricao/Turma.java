88647f6f7ed46d3d3bb5c54b67ea8533a568f8e8
Wilson Guimarães <wegneto@gmail.com>
2011-10-05 14:48:38 -0300

    Passo 2 - Junit - Logger injetado, lançando NullPointerException

--- inscricao/src/main/java/br/gov/serpro/inscricao/Turma.java
+++ inscricao/src/main/java/br/gov/serpro/inscricao/Turma.java
@@ -2,12 +2,20 @@ package br.gov.serpro.inscricao;
 
 import java.util.ArrayList;
 
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+
 public class Turma {
 
+	@Inject
+	private Logger logger;
+	
 	private ArrayList<String> alunos = new ArrayList<String>();
 
 	public void matricular(String aluno) {
 		alunos.add(aluno);
+		logger.info("Cadastro realizado com sucesso");
 	}
 
 	public boolean estaMatriculado(String aluno) {
