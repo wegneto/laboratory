57abf543a6746f73d7ebd2c6c4180469c3fbc434
Wilson Guimarães <wegneto@gmail.com>
2011-10-06 11:18:18 -0300

    Passo 17 - Modulo 4 - exception - Anotou TurmaException com @ApplicationException.

--- inscricao/src/main/java/br/gov/serpro/inscricao/TurmaException.java
+++ inscricao/src/main/java/br/gov/serpro/inscricao/TurmaException.java
@@ -1,7 +1,10 @@
 package br.gov.serpro.inscricao;
 
+import br.gov.frameworkdemoiselle.exception.ApplicationException;
+
+@ApplicationException(rollback = true)
 public class TurmaException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
-	
+
 }
