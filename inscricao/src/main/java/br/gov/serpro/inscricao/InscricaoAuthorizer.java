7f62b68e4740d78d1a4452cc5c38dab74e4e56b3
Wilson Guimarães <wegneto@gmail.com>
2011-10-06 16:19:10 -0300

    Passo 28 - Módulo 6 - Segurança - Criar classe que implementa a interface Authorizer, anotá-la como @Alternative e adicioná-la ao beans.xml. Retornar true caso o usuário seja “creuza”.

---
+++ inscricao/src/main/java/br/gov/serpro/inscricao/InscricaoAuthorizer.java
@@ -0,0 +1,34 @@
+package br.gov.serpro.inscricao;
+
+import javax.enterprise.inject.Alternative;
+import javax.inject.Inject;
+
+import br.gov.frameworkdemoiselle.security.Authorizer;
+import br.gov.frameworkdemoiselle.security.SecurityContext;
+
+@Alternative
+public class InscricaoAuthorizer implements Authorizer {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private SecurityContext securityContext;
+
+	@Override
+	public boolean hasPermission(String arg0, String arg1) {
+		boolean hasPermission = false;
+		
+		if (securityContext.getUser().getId().equals("creuza")) {
+			hasPermission = true;
+		}
+		
+		return hasPermission;
+	}
+
+	@Override
+	public boolean hasRole(String arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
