e3fb2db2554dbc833eff9e20f97c8a7a008649fe
Wilson Guimarães <wegneto@gmail.com>
2011-10-13 16:17:36 -0300

    Módulo 7, modificação do MB e da pagina para permitir o cadastro de alunos.

--- inscricao/src/main/java/br/gov/serpro/inscricao/view/TurmaMB.java
+++ inscricao/src/main/java/br/gov/serpro/inscricao/view/TurmaMB.java
@@ -14,8 +14,22 @@ public class TurmaMB {
 	@Inject
 	private TurmaBC bc;
 	
+	private String nomeAluno;
+	
 	public List<Aluno> getAlunosMatriculados() {
 		return bc.obterAlunosMatriculados();
+	}
+	
+	public void matricular() {
+		bc.matricular(new Aluno(getNomeAluno()));
+	}
+
+	public String getNomeAluno() {
+		return nomeAluno;
+	}
+
+	public void setNomeAluno(String nomeAluno) {
+		this.nomeAluno = nomeAluno;
 	}
 
 }
