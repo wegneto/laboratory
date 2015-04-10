9b1b10567815863462f8b55fcfb4c9b138a7a9b3
Wilson Guimaraes <wegneto@gmail.com>
2015-04-10 14:52:48 -0300

    Implementação do M3C3 (Atualizando os dados de um pessoa) do tutorial da versão 2.5 do Framework Demoiselle.

--- cadastro/src/main/webapp/js/controller/pessoa.js
+++ cadastro/src/main/webapp/js/controller/pessoa.js
@@ -1,92 +1,159 @@
 $(function() {
 
-	var id = getUrlParameterByName('id');
+	var id = obterParametroDaUrlPorNome('id');
 	$("#id").val(id)
 
 	if (id) {
-		PessoaProxy.obter(id).done(loadOk).fail(loadFailed);
+		PessoaProxy.obter(id).done(obterOk).fail(obterFalhou);
 	}
+	
+	$("#nome, #email, #telefone").on('change', function(){
+        $(this).parent().removeClass("has-error has-success");
+        $(".text-danger").parent().removeClass("has-error");
+        $(".text-danger").hide();
+        var campo = this;
+        if(id){
+            switch(campo.id){
+                case "nome":
+                    var pessoaPatch = { nome : campo.value };
+                    break;
+                case "email":
+                    var pessoaPatch = { email : campo.value };
+                    break;
+                case "telefone":
+                    var pessoaPatch = { telefone : campo.value };
+                    break;
+            }
+            PessoaProxy.atualizarParcial(id, pessoaPatch).done(function(){ atualizarParcialOk(campo.id) }).fail(atualizarFalhou);
+        }
+    });
+ 
+    $("#nome, #email, #telefone").on('blur', function(){
+        if($(this).parent().hasClass("has-error")){
+            $(this).focus();
+        }
+    });
 
-	$("form").submit(
-			function(event) {
-				/* Limpa as mensagens de erro */
-				$("#global-message").removeClass("alert-danger alert-success")
-						.empty().hide();
-				$(".text-danger").parent().removeClass("has-error");
-				$(".text-danger").hide();
-				event.preventDefault();
+	$("form").submit(function(event) {
+		/* Limpa as mensagens de erro */
+		$("#global-message").removeClass("alert-danger alert-success").empty().hide();
+		$(".control-label").parent().removeClass("has-success");
+		$(".text-danger").parent().removeClass("has-error");
+		$(".text-danger").hide();
 
-				var pessoa = {
-					nome : $("#nome").val(),
-					email : $("#email").val(),
-					telefone : $("#telefone").val()
-				};
+		event.preventDefault();
 
-				PessoaProxy.insert(pessoa).done(insertOk).fail(insertFail);
+		var pessoa = {
+			nome : $("#nome").val(),
+			email : $("#email").val(),
+			telefone : $("#telefone").val()
+		};
 
-			});
+		if (id) {
+			PessoaProxy.atualizar(id, pessoa).done(atualizarOk).fail(atualizarFalhou);
+		} else {
+			PessoaProxy.inserir(pessoa).done(inserirOk).fail(inserirFalhou);
+		}
+
+	});
 
 });
 
-function getUrlParameterByName(name) {
-	name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-	var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"), results = regex.exec(location.search);
-	return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+function atualizarParcialOk(id){
+    $("#" + id ).parent().addClass("has-success", {duration : 3000});
 }
 
-function loadOk(data) {
+function obterParametroDaUrlPorNome(name) {
+	name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+	var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"), results = regex.exec(location.search);
+	return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+function obterOk(data) {
 	$("#nome").val(data.nome);
 	$("#email").val(data.email);
 	$("#telefone").val(data.telefone);
 }
 
-function loadFailed(request) {
+function obterFalhou(request) {
 	switch (request.status) {
-	case 404:
-		alert("Você está tentando acessar um registro inexistente!");
-		break;
+		case 404:
+			alert("Você está tentando acessar um registro inexistente!");
+			break;
 
-	default:
-		break;
+		default:
+			break;
 	}
 }
 
-function insertOk(data, textStatus, jqXHR) {
-	console.log("Status: " + jqXHR.status);
-	console.log("Location: " + jqXHR.getResponseHeader('Location'));
-	$("#global-message").addClass("alert-success").text(
-			"Pessoa com id = " + data + " criado com sucesso.").show();
+function inserirOk(data, textStatus, jqXHR) {
+	$("#global-message").addClass("alert-success").text("Pessoa com id = " + data + " criado com sucesso.").show();
 }
 
-function insertFail(request) {
-	switch (request.status) {
-	case 422:
-		$($("form input").get().reverse()).each(function() {
-			var id = $(this).attr('id');
-			var message = null;
+function atualizarOk(data, textStatus, jqXHR) {
+	$("#global-message").addClass("alert-success").text("Pessoa atualizada com sucesso.").show();
+}
 
-			$.each(request.responseJSON, function(index, value) {
-				if (id == value.property) {
-					message = value.message;
-					return;
+function atualizarFalhou(request) {
+	switch (request.status) {
+		case 422:
+			$($("form input").get().reverse()).each(function() {
+				var id = $(this).attr('id');
+				var message = null;
+
+				$.each(request.responseJSON, function(index, value) {
+					if (id == value.property) {
+						message = value.message;
+						return;
+					}
+				});
+
+				if (message) {
+					console.log($("#" + id).parent());
+					$("#" + id).parent().addClass("has-error");
+					$("#" + id + "-message").html(message).show();
+					$(this).focus();
+				} else {
+					$("#" + id).parent().removeClass("has-error");
+					$("#" + id + "-message").hide();
 				}
 			});
+			break;
 
-			if (message) {
-				console.log($("#" + id).parent());
-				$("#" + id).parent().addClass("has-error");
-				$("#" + id + "-message").html(message).show();
-				$(this).focus();
-			} else {
-				$("#" + id).parent().removeClass("has-error");
-				$("#" + id + "-message").hide();
-			}
-		});
-		break;
+		default:
+			$("#global-message").addClass("alert-danger").text("Erro ao atualizar pessoa.").show();
+			break;
+	}
+}
 
-	default:
-		$("#global-message").addClass("alert-danger").text(
-				"Erro ao cadastrar pessoa.").show();
-		break;
+function inserirFalhou(request) {
+	switch (request.status) {
+		case 422:
+			$($("form input").get().reverse()).each(function() {
+				var id = $(this).attr('id');
+				var message = null;
+
+				$.each(request.responseJSON, function(index, value) {
+					if (id == value.property) {
+						message = value.message;
+						return;
+					}
+				});
+
+				if (message) {
+					console.log($("#" + id).parent());
+					$("#" + id).parent().addClass("has-error");
+					$("#" + id + "-message").html(message).show();
+					$(this).focus();
+				} else {
+					$("#" + id).parent().removeClass("has-error");
+					$("#" + id + "-message").hide();
+				}
+			});
+			break;
+
+		default:
+			$("#global-message").addClass("alert-danger").text("Erro ao cadastrar pessoa.").show();
+			break;
 	}
 }
--- cadastro/src/main/webapp/js/proxy/pessoa-proxy.js
+++ cadastro/src/main/webapp/js/proxy/pessoa-proxy.js
@@ -1,19 +1,38 @@
 var PessoaProxy = {
- 
-   url : "api/pessoas",
- 
-   insert : function(_pessoa) {
-    return $.ajax({
-        type : "POST",
-        url : this.url,
-        data : JSON.stringify(_pessoa),
-        contentType : "application/json"
-    });
-   },
-   obter : function(id){
-       return $.ajax({
-           type : "GET",
-           url : this.url + "/" + id
-       });
-   }
+
+	url : "api/pessoas",
+
+	inserir : function(_pessoa) {
+		return $.ajax({
+			type : "POST",
+			url : this.url,
+			data : JSON.stringify(_pessoa),
+			contentType : "application/json"
+		});
+	},
+
+	atualizar : function(id, pessoa) {
+		return $.ajax({
+			type : "PUT",
+			url : this.url + "/" + id,
+			data : JSON.stringify(pessoa),
+			contentType : "application/json"
+		})
+	},
+
+	atualizarParcial : function(id, pessoaPatch) {
+		return $.ajax({
+			type : "PATCH",
+			url : this.url + "/" + id,
+			data : JSON.stringify(pessoaPatch),
+			contentType : "application/json"
+		});
+	},
+
+	obter : function(id) {
+		return $.ajax({
+			type : "GET",
+			url : this.url + "/" + id
+		});
+	}
 };
