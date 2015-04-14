38562883c530234b7ddc59dc8cf691b8f6284cb1
Wilson Guimarães <wegneto@gmail.com>
2015-04-14 18:39:34 -0300

    Implementação da funcionalidade de leitura de múltiplas linhas.

--- codejam/src/main/java/codejam/MinimumScalarProduct.java
+++ codejam/src/main/java/codejam/MinimumScalarProduct.java
@@ -5,16 +5,36 @@ import java.util.Collections;
 
 public class MinimumScalarProduct {
 
-	public short calculate(short[] array1, short[] array2) {
+	public short treatInput(String input1, String input2) {
+		String[] array1 = input1.split(" ");
+		String[] array2 = input2.split(" ");
+
 		short result = 0;
-		
+
 		Arrays.sort(array1);
+		Arrays.sort(array2, Collections.reverseOrder());
 
 		for (int i = 0; i < array1.length; i++) {
-			result += (array1[i] * array2[i]);
+			result += (Short.parseShort(array1[i]) * Short.parseShort(array2[i]));
 		}
 
 		return result;
+	}
+
+	public String parseInput(String input) {
+		String[] s = input.split("\n");
+		int caseNumber = 1;
+
+		StringBuffer output = new StringBuffer();
+
+		for (int i = 1; i < s.length; i = i + 3) {
+			short result = this.treatInput(s[i + 1], s[i + 2]);
+
+			output.append("Case #").append(caseNumber).append(": ").append(result).append("\n");
+			caseNumber++;
+		}
+
+		return output.toString();
 	}
 
 }
--- codejam/src/test/java/codejam/MinimumScalarProductTest.java
+++ codejam/src/test/java/codejam/MinimumScalarProductTest.java
@@ -7,24 +7,43 @@ import org.junit.Test;
 public class MinimumScalarProductTest {
 
 	@Test
-	public void test1() {
+	public void treatInputWithSameValues() {
 		MinimumScalarProduct msp = new MinimumScalarProduct();
-		short result = msp.calculate(new short[] { 1, 1 }, new short[] { 1, 1 });
-		assertEquals(2, result);
+		short output = msp.treatInput("2 2", "2 2");
+		assertEquals(8, output);
 	}
 
 	@Test
-	public void test2() {
+	public void treatInputWithDifferentValues() {
 		MinimumScalarProduct msp = new MinimumScalarProduct();
-		short result = msp.calculate(new short[] { 1, 2 }, new short[] { 2, 1 });
-		assertEquals(4, result);
+		short output = msp.treatInput("2 3", "2 3");
+		assertEquals(12, output);
 	}
 
 	@Test
-	public void test3() {
+	public void treatInputWithDifferentValuesOutOfOrder() {
 		MinimumScalarProduct msp = new MinimumScalarProduct();
-		short result = msp.calculate(new short[] { 1, 3, -5 }, new short[] { -2, 4, 1 });
-		assertEquals(-25, result);
+		short output = msp.treatInput("1 3 -5", "-2 4 1");
+		assertEquals(-25, output);
+	}
+
+	@Test
+	public void treatMultipeLinesInput() {
+		StringBuffer input = new StringBuffer();
+		input.append("2\n");
+		input.append("3\n");
+		input.append("1 3 -5\n");
+		input.append("-2 4 1\n");
+		input.append("5\n");
+		input.append("1 2 3 4 5\n");
+		input.append("1 0 1 0 1\n");
+		String output = (new MinimumScalarProduct()).parseInput(input.toString());
+
+		StringBuffer expectedOutput = new StringBuffer();
+		expectedOutput.append("Case #1: -25\n");
+		expectedOutput.append("Case #2: 6\n");
+		
+		assertEquals(expectedOutput.toString(), output);
 	}
 
 }
