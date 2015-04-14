93504386e4eebbf27b030bd62c528a644cd91c27
Wilson Guimaraes <wegneto@gmail.com>
2015-04-14 17:01:43 -0300

    Implementação dos primeiros testes.

--- codejam/src/main/java/codejam/MinimumScalarProduct.java
+++ codejam/src/main/java/codejam/MinimumScalarProduct.java
@@ -1,10 +1,20 @@
 package codejam;
 
+import java.util.Arrays;
+import java.util.Collections;
 
 public class MinimumScalarProduct {
 
 	public short calculate(short[] array1, short[] array2) {
-		return 2;
+		short result = 0;
+		
+		Arrays.sort(array1);
+
+		for (int i = 0; i < array1.length; i++) {
+			result += (array1[i] * array2[i]);
+		}
+
+		return result;
 	}
 
 }
--- codejam/src/test/java/codejam/MinimumScalarProductTest.java
+++ codejam/src/test/java/codejam/MinimumScalarProductTest.java
@@ -4,16 +4,27 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-
 public class MinimumScalarProductTest {
-	
+
 	@Test
 	public void test1() {
-		short[] array1 = {1,1};
-		short[] array2 = {1,1};
 		MinimumScalarProduct msp = new MinimumScalarProduct();
-		short result = msp.calculate(array1, array2);
+		short result = msp.calculate(new short[] { 1, 1 }, new short[] { 1, 1 });
 		assertEquals(2, result);
+	}
+
+	@Test
+	public void test2() {
+		MinimumScalarProduct msp = new MinimumScalarProduct();
+		short result = msp.calculate(new short[] { 1, 2 }, new short[] { 2, 1 });
+		assertEquals(4, result);
+	}
+
+	@Test
+	public void test3() {
+		MinimumScalarProduct msp = new MinimumScalarProduct();
+		short result = msp.calculate(new short[] { 1, 3, -5 }, new short[] { -2, 4, 1 });
+		assertEquals(-25, result);
 	}
 
 }
