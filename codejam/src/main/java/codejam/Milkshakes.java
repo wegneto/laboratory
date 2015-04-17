1e4ffcf837205d5a82d6b5ebb3a0bc5d36e0c21b
Wilson Guimaraes <wegneto@gmail.com>
2015-04-17 16:58:25 -0300

    Construção dos primeiros casos de testes.

--- codejam/src/main/java/codejam/Milkshakes.java
+++ codejam/src/main/java/codejam/Milkshakes.java
@@ -2,7 +2,7 @@ package codejam;
 
 public class Milkshakes {
 
-	public String getOutput() {
+	public String getBatches() {
 		return "Case #1: 1 0 0 0 0";
 	}
 
--- codejam/src/test/java/codejam/MilkshakesTest.java
+++ codejam/src/test/java/codejam/MilkshakesTest.java
@@ -7,9 +7,15 @@ import org.junit.Test;
 public class MilkshakesTest {
 
 	@Test
-	public void test1() {
-		String output = (new Milkshakes()).getOutput();
+	public void getBatchesForThreeClientsAndFiveFlavors() {
+		String output = (new Milkshakes()).getBatches();
 		assertEquals("Case #1: 1 0 0 0 0", output);
+	}
+	
+	@Test
+	public void getBatchesForTwoClientsOneFlavorOneMalted() {
+		String output = (new Milkshakes()).getBatches();
+		assertEquals("Case #1: IMPOSSIBLE", output);
 	}
 
 }
