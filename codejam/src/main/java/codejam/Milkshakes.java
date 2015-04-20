4a65242c6281a49e775b989f217f4ff778282bc5
Wilson Guimarães <wegneto@gmail.com>
2015-04-20 20:58:41 -0300

    Criação de novos casos de testes.

--- codejam/src/main/java/codejam/Milkshakes.java
+++ codejam/src/main/java/codejam/Milkshakes.java
@@ -2,8 +2,24 @@ package codejam;
 
 public class Milkshakes {
 
-	public String getBatches() {
-		return "Case #1: 1 0 0 0 0";
-	}
+	public String getBatches(int numFlavors, int numCustomers, int[][] customerFlavors) {
+		int[] result = new int[numFlavors];
 
+		for (int customer = 0; customer < numCustomers; customer++) {
+			for (int flavor = 0; flavor < numFlavors; flavor++) {
+				for (int i = 0; i < customerFlavors[customer].length; i = i + 2) {
+					if (customerFlavors[customer][i] == (flavor + 1)) {
+						result[flavor] = customerFlavors[customer][i + 1];
+					}
+				}
+			}
+		}
+
+		StringBuilder output = new StringBuilder();
+		for (int i = 0; i < result.length; i++) {
+			output.append(result[i]).append(" ");
+		}
+
+		return "Case #1: " + output.toString();
+	}
 }
--- codejam/src/test/java/codejam/MilkshakesTest.java
+++ codejam/src/test/java/codejam/MilkshakesTest.java
@@ -7,15 +7,33 @@ import org.junit.Test;
 public class MilkshakesTest {
 
 	@Test
-	public void getBatchesForThreeClientsAndFiveFlavors() {
-		String output = (new Milkshakes()).getBatches();
-		assertEquals("Case #1: 1 0 0 0 0", output);
+	public void getBatchesForOneClientThatLikeOneFlavorUnmalted() {
+		String output = (new Milkshakes()).getBatches(1, 1, new int[][] { { 1, 0 } });
+		assertEquals("Case #1: 0 ", output);
+	}
+
+	@Test
+	public void getBatchesForOneClientThatLikeTwoFlavorsUnmalted() {
+		String output = (new Milkshakes()).getBatches(2, 1, new int[][] { { 1, 0, 2, 0 } });
+		assertEquals("Case #1: 0 0 ", output);
+	}
+
+	@Test
+	public void getBatchesForOneClientThatLikeTwoFlavorsOneMaltedBetweenFiveFlavors() {
+		String output = (new Milkshakes()).getBatches(5, 1, new int[][] { { 1, 0, 4, 1 } });
+		assertEquals("Case #1: 0 0 0 1 0 ", output);
+	}
+
+	@Test
+	public void getBatchesForTwoClientsThatLikeThreeFlavorsBetweenFiveFlavors() {
+		String output = (new Milkshakes()).getBatches(5, 2, new int[][] { { 1, 0 }, { 2, 0, 4, 0 } });
+		assertEquals("Case #1: 0 0 0 0 0 ", output);
 	}
 	
 	@Test
-	public void getBatchesForTwoClientsOneFlavorOneMalted() {
-		String output = (new Milkshakes()).getBatches();
-		assertEquals("Case #1: IMPOSSIBLE", output);
+	public void getBatchesForTwoClientsThatLikeFourFlavorsOneMaltedBetweenFiveFlavors() {
+		String output = (new Milkshakes()).getBatches(5, 2, new int[][] { { 1, 0, 3, 1 }, { 2, 0, 4, 0 } });
+		assertEquals("Case #1: 0 0 1 0 0 ", output);
 	}
 
 }
