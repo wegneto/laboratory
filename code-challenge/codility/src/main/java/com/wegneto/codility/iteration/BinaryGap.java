98b1fdefe97e84a3562dedbb22033fac52b9c34d
Wilson Guimarães <wegneto@gmail.com>
2016-08-16 18:01:30 -0300

    Ajustes de código.

--- code-challenge/codility/src/main/java/com/wegneto/codility/iteration/BinaryGap.java
+++ code-challenge/codility/src/main/java/com/wegneto/codility/iteration/BinaryGap.java
@@ -3,37 +3,28 @@ package com.wegneto.codility.iteration;
 public class BinaryGap {
 
 	public int solution(int N) {
-		String binary = "";
-		while (N >= 1) {
-			int resto = (N % 2);
-			N = N / 2;
-			binary = resto + binary;
-		}
-
-		binary = (binary.isEmpty() ? "00" : binary);
-
-		System.out.println(binary);
-		int found = 0;
+		String binary = Integer.toBinaryString(N);
+		
+		int max = 0;
 		int aux = 0;
 		
 		for (int i = 0; i < binary.length(); i++) {
 			if (binary.charAt(i) == '0') {
 				aux++;
 			} else if (aux > 0) {
-				if (aux > found) {
-					found = aux;
+				if (aux > max) {
+					max = aux;
 				}
 				aux = 0;
 			}
 			
 		}
-		System.out.println(found);
 		
-		return found;
+		return max;
 	}
-
+	
 	public static void main(String[] args) {
-		new BinaryGap().solution(35);
+		new BinaryGap().solution(1047);
 	}
 
 }
