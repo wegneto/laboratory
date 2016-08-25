872b1d02ccad30bc3163e51167a446c9a9d4eff9
Wilson Guimarães <wegneto@gmail.com>
2016-08-25 10:41:28 -0300

    Write a function that, given a zero-indexed array A, returns 1 if array A is a permutation and 0 if it is not

---
+++ code-challenge/codility/src/main/java/com/wegneto/codility/countingelements/PermCheck.java
@@ -0,0 +1,29 @@
+package com.wegneto.codility.countingelements;
+
+public class PermCheck {
+
+	public int solution(final int[] arr) {
+		final int N = arr.length;
+		int[] control = new int[N];
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] > N) {
+				return 0;
+			}
+			control[arr[i]-1]++;
+			if (control[arr[i]-1] > 1) {
+				return 0;
+			}
+		}
+
+		return 1;//(gaussSum == sum ? 1 : 0);
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new PermCheck().solution(new int[] { 4, 1, 3, 2 }));
+		System.out.println(new PermCheck().solution(new int[] { 4, 1, 3 }));
+		System.out.println(new PermCheck().solution(new int[] { 1, 4, 1 }));
+		System.out.println(new PermCheck().solution(new int[] { 1, 1, 3 }));
+	}
+
+}
