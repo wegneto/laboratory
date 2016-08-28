2db0b5009b01450797dfe1500933c561d3cb3ef7
Wilson Guimarães <wegneto@gmail.com>
2016-08-27 22:28:30 -0300

    Refactoring.

--- code-challenge/codility/src/main/java/com/wegneto/codility/countingelements/MissingInteger.java
+++ code-challenge/codility/src/main/java/com/wegneto/codility/countingelements/MissingInteger.java
@@ -3,24 +3,26 @@ package com.wegneto.codility.countingelements;
 public class MissingInteger {
 
 	public int solution(int[] arr) {
-		int minSoFar = 1;
-		int i = 0;
-		
-		while (i < arr.length) {
-			if (arr[i] == minSoFar) {
-				minSoFar++;
-				i = 0;
+		boolean[] test = new boolean[arr.length + 1];
+
+		for (int j = 0; j < arr.length; j++) {
+			if (arr[j] > 0 && arr[j] < arr.length + 1) {
+				test[arr[j] - 1] = true;
 			}
-			i++;
 		}
-		
-		System.out.println(minSoFar);
-		
-		return minSoFar;
+
+		for (int j = 0; j < test.length; j++) {
+			if (test[j] == false) {
+				return j + 1;
+			}
+		}
+
+		return 0;
 	}
 
 	public static void main(String[] args) {
 		new MissingInteger().solution(new int[] { 1, 3, 6, 4, 1, 2 });
+		new MissingInteger().solution(new int[] { 1 });
 	}
 
 }
