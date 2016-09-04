e653b628a5b2bef06be035a0b0311c83fd58f652
Wilson Guimarães <wegneto@gmail.com>
2016-09-04 18:05:00 -0300

    Resolvendo o problema de performance.

--- code-challenge/codility/src/main/java/com/wegneto/codility/countingelements/MaxCounters.java
+++ code-challenge/codility/src/main/java/com/wegneto/codility/countingelements/MaxCounters.java
@@ -1,34 +1,33 @@
 package com.wegneto.codility.countingelements;
 
-import java.util.Arrays;
-
 public class MaxCounters {
 
 	public int[] solution(int N, int[] A) {
 		int[] result = new int[N];
-		int maxValue = 0;
+		int currentMax = 0, maxCounter = 0;
 
 		for (int k = 0; k < A.length; k++) {
 			if (A[k] >= 1 && A[k] <= N) {
 				// increase(X)
-				result[A[k]-1]++;
-				
-				if (result[A[k]-1] > maxValue) {
-					maxValue = result[A[k]-1];
+				if (result[A[k] - 1] < maxCounter) {
+					result[A[k] - 1] = maxCounter;
 				}
-				
-				
+
+				result[A[k] - 1]++;
+
+				if (result[A[k] - 1] > currentMax) {
+					currentMax = result[A[k] - 1];
+				}
 			} else if (A[k] == (N + 1)) {
 				// maxcounter
-				Arrays.fill(result, maxValue);
+				maxCounter = currentMax;
 			}
-			
-			for (int i = 0; i < result.length; i++) {
-				System.out.print(result[i] + " ");
+		}
+
+		for (int i = 0; i < result.length; i++) {
+			if (result[i] < maxCounter) {
+				result[i] = maxCounter;
 			}
-			
-			System.out.println(" ");
-			
 		}
 
 		return result;
@@ -36,6 +35,7 @@ public class MaxCounters {
 
 	public static void main(String[] args) {
 		new MaxCounters().solution(5, new int[] { 3, 4, 4, 6, 1, 4, 4 });
+		new MaxCounters().solution(5, new int[] { 3, 4, 4, 6, 1, 4, 6 });
 	}
 
 }
