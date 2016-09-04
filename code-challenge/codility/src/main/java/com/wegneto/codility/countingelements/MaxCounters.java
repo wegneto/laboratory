8d84171c282a5e26171941dbe7cefe6587974344
Wilson Guimarães <wegneto@gmail.com>
2016-09-03 23:56:34 -0300

    Criação da classe MaxCounters

---
+++ code-challenge/codility/src/main/java/com/wegneto/codility/countingelements/MaxCounters.java
@@ -0,0 +1,13 @@
+package com.wegneto.codility.countingelements;
+
+public class MaxCounters {
+	
+	public int[] solution(int N, int[] A) {
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		new MaxCounters().solution(0, new int[]{});
+	}
+
+}
