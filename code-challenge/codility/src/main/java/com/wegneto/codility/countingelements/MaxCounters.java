4cc80ee473a99651f17fa2bbc710b93b5abd6d38
Wilson Guimarães <wegneto@gmail.com>
2016-09-04 00:56:58 -0300

    Solução do problema MaxCounters

--- code-challenge/codility/src/main/java/com/wegneto/codility/countingelements/MaxCounters.java
+++ code-challenge/codility/src/main/java/com/wegneto/codility/countingelements/MaxCounters.java
@@ -1,13 +1,41 @@
 package com.wegneto.codility.countingelements;
 
+import java.util.Arrays;
+
 public class MaxCounters {
-	
+
 	public int[] solution(int N, int[] A) {
-		return null;
+		int[] result = new int[N];
+		int maxValue = 0;
+
+		for (int k = 0; k < A.length; k++) {
+			if (A[k] >= 1 && A[k] <= N) {
+				// increase(X)
+				result[A[k]-1]++;
+				
+				if (result[A[k]-1] > maxValue) {
+					maxValue = result[A[k]-1];
+				}
+				
+				
+			} else if (A[k] == (N + 1)) {
+				// maxcounter
+				Arrays.fill(result, maxValue);
+			}
+			
+			for (int i = 0; i < result.length; i++) {
+				System.out.print(result[i] + " ");
+			}
+			
+			System.out.println(" ");
+			
+		}
+
+		return result;
 	}
-	
+
 	public static void main(String[] args) {
-		new MaxCounters().solution(0, new int[]{});
+		new MaxCounters().solution(5, new int[] { 3, 4, 4, 6, 1, 4, 4 });
 	}
 
 }
