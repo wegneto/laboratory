2f58d1cf46a2059d7cb0d81a2714455066bb0558
Wilson Guimarães <wegneto@gmail.com>
2016-09-14 00:04:05 -0300

    Prefix Sum

---
+++ code-challenge/codility/src/main/java/com/wegneto/codility/prefixsum/PrefixSum.java
@@ -0,0 +1,27 @@
+package com.wegneto.codility.prefixsum;
+
+public class PrefixSum {
+
+	private int[] getPrefixSum(int[] array) {
+		int n = array.length;
+		int[] P = new int[n+1];
+		
+		for (int i = 1; i < P.length; i++) {
+			P[i] = P[i-1] + array[i-1];
+			System.out.print(P[i] +" ");
+		}
+		
+		return P;
+	}
+	
+	private int countTotal(int[] P, int x, int y) {
+		return P[y + 1] - P[x];	
+	}
+	
+	public static void main(String[] args) {
+		PrefixSum prefixSum = new PrefixSum();
+		int[] P = prefixSum.getPrefixSum(new int[] {0, 1, 2, 3, 4} );
+		System.out.println(prefixSum.countTotal(P, 3, 4));
+	}
+	
+}
